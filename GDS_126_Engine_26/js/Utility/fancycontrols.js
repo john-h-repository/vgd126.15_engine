e6c0cb02164a09bf425b1102fef87f833f181293
john-h-repository <lnpirrotta@email.neit.edu>
2026-01-29 10:06:29 -0500

    Delete GDS_126_Engine_26 directory

--- GDS_126_Engine_26/js/Utility/fancycontrols.js
+++
@@ -1,5 +0,0 @@
-var keys={};
-var pressed=false;
-document.addEventListener("keydown", (e)=>{pressed=true; keys[String.fromCharCode(e.keyCode)]=true});
-document.addEventListener("keyup", (e)=>{pressed=false; keys[String.fromCharCode(e.keyCode)]=false});
-
