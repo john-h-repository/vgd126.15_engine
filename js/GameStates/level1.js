150d4b6772f448edfb065091818c9745a83d7591
john-h-repository <lnpirrotta@email.neit.edu>
2026-02-26 09:16:34 -0500

    wow

--- js/GameStates/level1.js
+++ js/GameStates/level1.js
@@ -60,7 +60,7 @@ levelItems.add([caveBack.grid, ground, plat, cave.grid]);
 
 //Very back background
 var sky = new GameObject({width:canvas.width, height:canvas.height, color:"cyan"})
-sky.img.src = `images/sky.png`
+sky.img.src = `images/bg.png`
 
 /*
  	//Not used, unless you want a 4th level of paralax
@@ -70,11 +70,11 @@ sky.img.src = `images/sky.png`
 
 //repeating background
 var rbg = new GameObject({x:level.x, y:level.y, width:1024, height:512})
-rbg.img.src=`images/hills.png`
+rbg.img.src=`images/bgrepeat.png`
 
 //middleground
 var bg = new GameObject({x:level.x,y:level.y, width:canvas.width*4, height:canvas.height})
-bg.img.src=`images/bgfull.png`
+bg.img.src=`images/middleground.png`
 
 /*------------------vvBULLET STUFFvv----------------------*/
 
